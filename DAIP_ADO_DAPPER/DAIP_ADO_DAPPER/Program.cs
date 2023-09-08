8188d317f9759441c6d4cd3bd5b90ba993f16cbb
KHumoyunmirzo <127532144+Humoyunmirzo-coder@users.noreply.github.com>
2023-09-08 22:33:57 +0500

    d

---
+++ DAIP_ADO_DAPPER/DAIP_ADO_DAPPER/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DAIP_ADO_DAPPER
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
